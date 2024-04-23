7b56c189ebeff4f09174655ad10aca598d4b4ee0
Adam Berlinger <adam.berlinger@st.com>
2024-04-23 10:17:35 +0200

    boards: add support for H735 and H750 for examples

--- boards/st/stm32h735g_disco/board.cmake
+++ boards/st/stm32h735g_disco/board.cmake
@@ -3,6 +3,8 @@
 
 board_runner_args(jlink "--device=STM32H735IG" "--speed=4000")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw" "--extload=MX25LM51245G_STM32H735G-DK.stldr")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
