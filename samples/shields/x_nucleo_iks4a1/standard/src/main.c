cec90360e8f54475c05296e43cd3466a9a77f9fb
Adam BERLINGER <adam.berlinger@st.com>
2025-09-26 11:28:02 +0200

    Modify the X_Nucleo_IKS4A1 example to support I3C
    Triggers from sensor are disabled, since they use IBI (in band interrupt)
    and it doesn't work at the moment.

--- samples/shields/x_nucleo_iks4a1/standard/src/main.c
+++ samples/shields/x_nucleo_iks4a1/standard/src/main.c
@@ -198,7 +198,7 @@ static void lsm6dsv16x_config(const struct device *lsm6dsv16x)
 
 	trig.type = SENSOR_TRIG_DATA_READY;
 	trig.chan = SENSOR_CHAN_ACCEL_XYZ;
-	sensor_trigger_set(lsm6dsv16x, &trig, lsm6dsv16x_acc_trig_handler);
+	//sensor_trigger_set(lsm6dsv16x, &trig, lsm6dsv16x_acc_trig_handler);
 #endif
 }
 
@@ -221,7 +221,7 @@ static void lps22df_config(const struct device *lps22df)
 
 	trig.type = SENSOR_TRIG_DATA_READY;
 	trig.chan = SENSOR_CHAN_ALL;
-	sensor_trigger_set(lps22df, &trig, lps22df_trigger_handler);
+	//sensor_trigger_set(lps22df, &trig, lps22df_trigger_handler);
 #endif
 }
 
@@ -272,7 +272,7 @@ int main(void)
 	const struct device *const lsm6dso16is = DEVICE_DT_GET_ONE(st_lsm6dso16is);
 	const struct device *const lsm6dsv16x = DEVICE_DT_GET_ONE(st_lsm6dsv16x);
 	const struct device *const lps22df = DEVICE_DT_GET_ONE(st_lps22df);
-	const struct device *const lis2duxs12 = DEVICE_DT_GET_ONE(st_lis2duxs12);
+	//const struct device *const lis2duxs12 = DEVICE_DT_GET_ONE(st_lis2duxs12);
 	int cnt = 1;
 
 	if (!device_is_ready(lsm6dso16is)) {
@@ -291,16 +291,16 @@ int main(void)
 		printk("%s: device not ready.\n", lps22df->name);
 		return 0;
 	}
-	if (!device_is_ready(lis2duxs12)) {
+	/*if (!device_is_ready(lis2duxs12)) {
 		printk("%s: device not ready.\n", lis2duxs12->name);
 		return 0;
-	}
+	}*/
 
 	lis2mdl_config(lis2mdl);
 	lsm6dso16is_config(lsm6dso16is);
 	lsm6dsv16x_config(lsm6dsv16x);
 	lps22df_config(lps22df);
-	lis2duxs12_config(lis2duxs12);
+	//lis2duxs12_config(lis2duxs12);
 
 	while (1) {
 		/* Get sensor samples */
@@ -330,10 +330,10 @@ int main(void)
 		}
 #endif
 #ifndef CONFIG_LIS2DUX12_TRIGGER
-		if (sensor_sample_fetch(lis2duxs12) < 0) {
+		/*if (sensor_sample_fetch(lis2duxs12) < 0) {
 			printf("LIS2DUXS12 XL Sensor sample update error\n");
 			return 0;
-		}
+		}*/
 #endif
 
 		/* Get sensor data */
@@ -353,12 +353,12 @@ int main(void)
 		sensor_channel_get(lps22df, SENSOR_CHAN_PRESS, &lps22df_press);
 		sensor_channel_get(lps22df, SENSOR_CHAN_AMBIENT_TEMP, &lps22df_temp);
 
-		sensor_channel_get(lis2duxs12, SENSOR_CHAN_ACCEL_XYZ, lis2duxs12_xl);
-		sensor_channel_get(lis2duxs12, SENSOR_CHAN_DIE_TEMP, &lis2duxs12_temp);
+		/*sensor_channel_get(lis2duxs12, SENSOR_CHAN_ACCEL_XYZ, lis2duxs12_xl);
+		sensor_channel_get(lis2duxs12, SENSOR_CHAN_DIE_TEMP, &lis2duxs12_temp);*/
 		/* Display sensor data */
 
 		/* Erase previous */
-		printf("\0033\014");
+		//printf("\0033\014");
 
 		printf("X-NUCLEO-IKS4A1 sensor dashboard\n\n");
 
@@ -406,13 +406,13 @@ int main(void)
 		printf("LPS22DF: Temperature: %.1f C\n", sensor_value_to_double(&lps22df_temp));
 		printf("LPS22DF: Pressure:%.3f kpa\n", sensor_value_to_double(&lps22df_press));
 
-		printf("LIS2DUXS12: Accel (m.s-2): x: %.3f, y: %.3f, z: %.3f\n",
+		/*printf("LIS2DUXS12: Accel (m.s-2): x: %.3f, y: %.3f, z: %.3f\n",
 			sensor_value_to_double(&lis2duxs12_xl[0]),
 			sensor_value_to_double(&lis2duxs12_xl[1]),
 			sensor_value_to_double(&lis2duxs12_xl[2]));
 
 		printf("LIS2DUXS12: Temperature: %.1f C\n",
-		       sensor_value_to_double(&lis2duxs12_temp));
+		       sensor_value_to_double(&lis2duxs12_temp));*/
 
 #if defined(CONFIG_LIS2MDL_TRIGGER)
 		printk("%d: lis2mdl trig %d\n", cnt, lis2mdl_trig_cnt);
@@ -434,5 +434,7 @@ int main(void)
 
 		cnt++;
 		k_sleep(K_MSEC(2000));
+		sensor_sample_fetch_chan(lps22df, SENSOR_CHAN_ALL);
+		sensor_sample_fetch_chan(lsm6dsv16x, SENSOR_CHAN_ALL);
 	}
 }
